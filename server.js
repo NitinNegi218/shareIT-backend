47549a4b17f709e1e68670eeae205f76c6b5e441
Nitin <nitinnegi27.nn@gmail.com>
2023-11-14 00:12:09 +0530

    cors error

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const path = require('path');
 const cors = require('cors');
 // Cors 
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+  origin: ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300', ...process.env.ALLOWED_CLIENTS.split(',')]
+};
 
 // Default configuration looks like
 // {
